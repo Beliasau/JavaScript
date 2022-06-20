ba88f990e19e2e491fdb2770ae1bc97e4d047dda
Пользователь <sanjaquatro@mail.ru>
2022-06-20 17:38:58 +0300

    new homework

---
+++ HW_2_course_Ksendzov/hw_2_js.js
@@ -0,0 +1,16 @@
+
+// a = 2
+// b = 1
+
+// while (b <= 10){
+//     console.log("2^" + b + " =", Math.pow(a,b))
+//     b++
+// }
+
+
+let c = ":)"
+
+for (var i = 0; i < 5; i++){
+    console.log(c)
+    c = c + ":)"
+}

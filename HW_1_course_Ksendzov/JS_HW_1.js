75506bb252d144e4e73cd2b92aaa9ebb4ff742fd
Пользователь <sanjaquatro@mail.ru>
2022-06-23 00:20:57 +0300

    completed

--- HW_1_course_Ksendzov/JS_HW_1.js
+++ HW_1_course_Ksendzov/JS_HW_1.js
@@ -88,9 +88,9 @@
 
  //22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 
-	let item_7 = item_6 + ""
+	let item_7 = item_6 + ""				//				let item_7 = String(item_6)
 
-	console.log("item_7 " + item_7 + " " + typeof(item_7))
+			console.log("item_7 " + item_7 + " " + typeof(item_7))
 
 
  //23. Создать переменную item_7_type
@@ -138,3 +138,150 @@
 	else{console.log("Technical work")}
 
 //==========================================================================================================================
+
+
+
+// JS. HW_1* 
+// Задания с разным количеством звездочек:)
+// 1*:
+// Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+// Пример: const checkAge = function(age) {
+// Ваши преобразования
+// }
+// Вывести в консоль результат работы функции с возрастами 17, 18, 61
+
+
+
+
+let age_2 = 18
+let age_3 = 60
+
+const checkAge = function(age_1){
+if(age_1 < age_2){
+    console.log("You don’t have access cause your age is " + age_1 + " It’s less then ")}
+   else if(age_1 >= age_2 && age_1 < age_3){console.log("Welcome  !")}
+   else if(age_1 > age_3){console.log("Keep calm and look Culture channel")}
+   else{console.log("Technical work")
+}
+}
+
+checkAge(17)
+checkAge(18)
+checkAge(61)
+
+
+
+
+
+// 2*:
+// Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+
+
+
+
+let age_2 = 18
+let age_3 = 60
+
+const checkAge = function(age_1) {
+ if(typeof age_1 == "number") {   
+        if(age_1 < age_2) {
+            console.log("You don’t have access cause your age is " + age_1 + " It’s less then ")
+    } else if(age_1 >= age_2 && age_1 < age_3){
+        console.log("Welcome  !")
+    } else if(age_1 > age_3){
+        console.log("Keep calm and look Culture channel")  
+    } else {
+    console.log("Technical work")
+    }
+} else console.log("Not an integer value")
+}
+
+    checkAge(17)
+    checkAge(18)
+    checkAge(61)
+    checkAge("sdg")
+
+
+
+
+
+// 3**:
+// Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+
+
+
+
+let age_2 = 18
+let age_3 = 60
+
+const checkAge = function(age_1) {
+if(age_1 && !isNaN(age_1)){
+           if(age_1 < age_2) {
+               console.log("You don’t have access cause your age is " + age_1 + " It’s less then ")
+       } else if(age_1 >= age_2 && age_1 < age_3){
+           console.log("Welcome  !")
+       } else if(age_1 > age_3){
+           console.log("Keep calm and look Culture channel")  
+       } else {
+       console.log("Technical work")
+       }
+   } else console.log("Not an integer value")
+   }
+   
+       checkAge(17)
+       checkAge(18)
+       checkAge(61)
+       checkAge("sdg")
+       checkAge("0")
+       checkAge("20")
+       checkAge("")
+
+
+
+
+
+// 4***:
+// Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="./hw1_JsForHTML.js"></script>
+</body>
+</html>
+
+
+
+// ==================
+
+
+
+const age_2 = 18
+const age_3 = 60
+
+const checkAge = function(age_1) {
+    if(age_1 && !isNaN(age_1)){
+               if(age_1 < age_2) {
+                   alert("You don’t have access cause your age is " + age_1 + " It’s less then ")
+           } else if(age_1 >= age_2 && age_1 < age_3){
+               alert("Welcome  !")
+           } else if(age_1 > age_3){
+               alert("Keep calm and look Culture channel")  
+           } else {
+           alert("Technical work")
+           }
+       } else alert("Not an integer value")
+       }
+
+      
+       let q = prompt('Enter your age')
+
+       checkAge(q)
